d402c48bae716d5f3771b7ee86917801711dbd66
yoneda <nsshowdream@gmail.com>
2018-12-28 19:22:49 +0900

    make it possible to read css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import styles from "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <p>hello, react</p>
+        <p className={styles.test}>my name is yoneda</p>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   entry: "./src/index.js", // 出力元
   output:{
@@ -13,10 +14,14 @@ module.exports = {
   module:{
     rules:[
       {
+        test: /\.css$/,
+        loaders: ["style-loader","css-loader?modules"],
+      },
+      {
         test:/\.js/,
         exclude:/node_modules/,
         use:["babel-loader"]
-      }
+      },
     ]
   },
 };
